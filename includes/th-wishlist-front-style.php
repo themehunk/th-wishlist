0d9bb350df83240c4f9e5b8a62976755dbe0ca63
shivam kushwah <37370154+devkushwahshivam@users.noreply.github.com>
2025-07-28 15:56:38 +0530

    dynamic style concanat remove

--- includes/th-wishlist-front-style.php
+++ includes/th-wishlist-front-style.php
@@ -8,13 +8,9 @@ if ( ! defined( 'ABSPATH' ) ) {
  */
 function thwl_front_style() {
 
-	$custom_css = '';
-
-	// Get saved settings with fallback defaults
 	$th_wishlist_option = get_option( 'thwl_settings', THWL_Settings::thwl_get_default_settings() );
 
-	// Button CSS
-	$custom_css .= "
+	return $custom_css = "
 	.thw-btn-custom-style .thw-add-to-wishlist-button .thw-icon {
 	color: " . esc_html($th_wishlist_option['th_wishlist_add_icon_color']) . ";
 	}
@@ -25,19 +21,11 @@ function thwl_front_style() {
 	color: " . esc_html($th_wishlist_option['th_wishlist_btn_txt_color']) . ";
 	background: " . esc_html($th_wishlist_option['th_wishlist_btn_bg_color']) . ";
 	}
-	";
-
-	// Table button colors
-	$custom_css .= "
 	.thw-table-custom-style .thw-wishlist-actions .thw-add-all-to-cart,
 	.thw-table-custom-style .thw-add-to-cart-cell .button {
 		color: " . esc_html($th_wishlist_option['th_wishlist_tb_btn_txt_color']) . ";
 		background: " . esc_html($th_wishlist_option['th_wishlist_tb_btn_bg_color']) . ";
 	}
-	";
-
-	// Table background and text colors
-	$custom_css .= "
 	.thw-table-custom-style .thw-wishlist-table {
 	    color: " . esc_html($th_wishlist_option['th_wishlist_table_txt_color']) . ";
 		background: " . esc_html($th_wishlist_option['th_wishlist_table_bg_color']) . ";
@@ -46,53 +34,41 @@ function thwl_front_style() {
 	.thw-table-custom-style .thw-wishlist-table td {
 		border-color: " . esc_html($th_wishlist_option['th_wishlist_table_brd_color']) . ";
 	}
-	";
-
-	// Assuming $th_wishlist_option contains already sanitized or trusted color values
-
-	$custom_css .= "
-	.thw-table-custom-style .thw-social-share a.thw-share-facebook {
+		.thw-table-custom-style .thw-social-share a.thw-share-facebook {
 		color: " . esc_html($th_wishlist_option['th_wishlist_shr_fb_color']) . ";
 	}
 	.thw-table-custom-style .thw-social-share a.thw-share-facebook:hover {
 		color: " . esc_html($th_wishlist_option['th_wishlist_shr_fb_hvr_color']) . ";
 	}
-	";
-
-	$custom_css .= "
-	.thw-table-custom-style .thw-social-share a.thw-share-twitter {
+		.thw-table-custom-style .thw-social-share a.thw-share-twitter {
 		color: " . esc_html($th_wishlist_option['th_wishlist_shr_x_color']) . ";
 	}
 	.thw-table-custom-style .thw-social-share a.thw-share-twitter:hover {
 		color: " . esc_html($th_wishlist_option['th_wishlist_shr_x_hvr_color']) . ";
 	}
-	";
-
-	$custom_css .= "
-	.thw-table-custom-style .thw-social-share a.thw-share-whatsapp {
+		.thw-table-custom-style .thw-social-share a.thw-share-twitter {
+		color: " . esc_html($th_wishlist_option['th_wishlist_shr_x_color']) . ";
+	}
+	.thw-table-custom-style .thw-social-share a.thw-share-twitter:hover {
+		color: " . esc_html($th_wishlist_option['th_wishlist_shr_x_hvr_color']) . ";
+	}
+		.thw-table-custom-style .thw-social-share a.thw-share-whatsapp {
 		color: " . esc_html($th_wishlist_option['th_wishlist_shr_w_color']) . ";
 	}
 	.thw-table-custom-style .thw-social-share a.thw-share-whatsapp:hover {
 		color: " . esc_html($th_wishlist_option['th_wishlist_shr_w_hvr_color']) . ";
 	}
-	";
-
-	$custom_css .= "
-	.thw-table-custom-style .thw-social-share a.thw-share-email {
+		.thw-table-custom-style .thw-social-share a.thw-share-email {
 		color: " . esc_html($th_wishlist_option['th_wishlist_shr_e_color']) . ";
 	}
 	.thw-table-custom-style .thw-social-share a.thw-share-email:hover {
 		color: " . esc_html($th_wishlist_option['th_wishlist_shr_e_hvr_color']) . ";
 	}
-	";
-
-	$custom_css .= "
-	.thw-table-custom-style .thw-social-share a.thw-copy-link-button {
+		.thw-table-custom-style .thw-social-share a.thw-copy-link-button {
 		color: " . esc_html($th_wishlist_option['th_wishlist_shr_c_color']) . ";
 	}
 	.thw-table-custom-style .thw-social-share a.thw-copy-link-button:hover {
 		color: " . esc_html($th_wishlist_option['th_wishlist_shr_c_hvr_color']) . ";
 	}
 	";
-	return $custom_css;
 }
